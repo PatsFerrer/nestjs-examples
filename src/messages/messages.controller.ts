bd0d92cc708a54dece2779492addf8ba9749c300
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-14 09:39:25 -0300

    feat: chamar service no controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,23 +1,33 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { createMessageDto } from './dtos/create-message.dto';
+import { MessagesServices } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
 
+  //crie uma instancia do service
+  messagesService: MessagesServices;
+
+  constructor() {
+    // DONT DO THIS ON REAL APP
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesServices();
+  }
+
+
   @Get()
   listMessages() {
-
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: createMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
-    
+    return this.messagesService.findOne(id);
   }
 
 }
