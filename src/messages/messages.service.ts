94210aebe491f85c1b4cfabb6128ff3a7134d99d
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-14 14:33:48 -0300

    refactor: adicionar injeção de dependencias completamente

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,8 +1,10 @@
+import { Injectable } from "@nestjs/common";
 import { MessagesRepository } from "./messages.repository";
 
+@Injectable()
 export class MessagesServices {
 
-  constructor(public messagesRepo: MessagesRepository) {}
+  constructor(public messagesRepo: MessagesRepository) { }
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
